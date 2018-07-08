6d77652769fbf6458baf1cb1cd93ac7275ee0415
49257620 <49257620@qq.com>
2018-07-08 17:19:27 +0800

    asset app 增加echart 资源监控图

--- asset/urls.py
+++ asset/urls.py
@@ -6,4 +6,5 @@ app_name = 'asset'
 urlpatterns = [
     path('', views.index, name="index"),
     path('list_ajax/', views.list_ajax, name="list_ajax"),
+    path('monitor_ajax/', views.monitor_ajax, name="monitor_ajax"),
 ]
--- asset/views.py
+++ asset/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from .models import Host
+from .models import Host,Resource
+from django.utils import timezone
+from datetime import timedelta
 
 
 # Create your views here.
@@ -13,3 +15,18 @@ def index(request):
 def list_ajax(request):
     result = [host.as_dict() for host in Host.objects.all()]
     return JsonResponse({"code": 200, "result": result})
+
+
+def monitor_ajax(request):
+    ip = request.GET.get('ip')
+    xAxis = []
+    CPU_data = []
+    MEM_data = []
+    start_time = timezone.now() - timedelta(days=1)
+    print(ip)
+    resources = Resource.objects.filter(collect_time__gte=start_time,ip=ip).order_by('collect_time')
+    for r in resources:
+        xAxis.append(r.collect_time.strftime('%Y-%m-%d %H:%M'))
+        CPU_data.append(r.cpu)
+        MEM_data.append(r.mem)
+    return JsonResponse({"code": 200, "result": {'xAxis':xAxis,'cpu':CPU_data,'mem':MEM_data}})
