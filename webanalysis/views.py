4015fddd3faa474861e73ed75a4439f47c4e5bda
49257620 <49257620@qq.com>
2018-07-15 12:12:18 +0800

    asset webanalysis 文件上传

--- webanalysis/views.py
+++ webanalysis/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import os
+from django.conf import settings
+import time
 
 # Create your views here.
 
 def index(request):
     return render(request, 'webanalysis/index.html')
+
+
+def upload(request):
+    upload_file = request.FILES.get('log_file',None)
+    #print(dir(upload_file))
+    upload_path = os.path.join(settings.BASE_DIR,'media','uploads',str(time.time()))
+    with open(upload_path,'wb') as fh:
+        for chunk in upload_file.chunks():
+            fh.write(chunk)
+    return HttpResponse('upload ok')
