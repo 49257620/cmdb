20858f881b75a409a2d74a6e005e0d9ff70a5ec0
49257620 <49257620@qq.com>
2018-07-22 16:41:37 +0800

    django rest api

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from .views import v1
 app_name = 'api'
 
 urlpatterns = [
-    path('v1/client/<uuid>/', v1.ViewClient.as_view(), name="client"),
+    path('v1/client/<ip>/', v1.ViewClient.as_view(), name="client"),
+    path('v1/client/<ip>/resource/', v1.ViewResource.as_view(), name="resource"),
 ]
--- api/views/v1.py
+++ api/views/v1.py
@@ -3,26 +3,56 @@
 # author = ‘LW’
 
 from django.views.generic import View
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import json
+from asset.models import Host, Resource
 
 
-class ViewClient(View):
-
-    def get(self, request, *args, **kwargs):
-        print(request.GET)
-        print(request.POST)
-        print(request.body)
-        print(args, kwargs)
-        return HttpResponse('get ok')
-
+class APIView(View):
     @csrf_exempt
     def dispatch(self, request, *args, **kwargs):
-        return super(ViewClient, self).dispatch(request, *args, **kwargs)
+        return super(APIView, self).dispatch(request, *args, **kwargs)
+
+    def get_json(self):
+        try:
+            return json.loads(self.request.body)
+        except BaseException as e:
+            return {}
+
+    def respones(self, result=None, code=200, errors={}):
+        return JsonResponse({'code': code, 'result': result, 'errors': errors})
+
+
+class ViewClient(APIView):
 
     def post(self, request, *args, **kwargs):
-        print(request.GET)
-        print(request.POST)
-        print(request.body)
-        print(args, kwargs)
-        return HttpResponse('post ok')
+        _ip = kwargs.get('ip', '')
+        _json = self.get_json()
+
+        ip = _ip
+        name = _json.get('name', '')
+        mac = _json.get('mac', '')
+        os = _json.get('os', '')
+        arch = _json.get('arch', '')
+        mem = _json.get('mem', 0)
+        cpu = _json.get('cpu', 0)
+        disk = _json.get('disk', '{}')
+        host = Host.create_or_replace(ip, name, mac, os, arch, mem, cpu, disk)
+
+        return self.respones(host.as_dict())
+
+
+class ViewResource(APIView):
+
+    def post(self, request, *args, **kwargs):
+        _ip = kwargs.get('ip', '')
+        _json = self.get_json()
+
+        record = Resource()
+        record.ip = _ip
+        record.cpu = _json.get('cpu', 0)
+        record.mem = _json.get('mem', 0)
+        record.save()
+
+        return self.respones()
